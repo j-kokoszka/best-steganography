2f7e2d8e9019e0fcd75abe4a6f427652e1d44bea
Jakub Kokoszka <jakub.kokoszka@softwaremind.com>
2024-04-20 18:33:20 +0200

    add checksum calculation and requirements

--- checker_app.py
+++ checker_app.py
@@ -2,17 +2,18 @@ import scapy.all as scapy
 import argparse
 import re
 import base64
+import hashlib
 
+# Calculating sha256 checksum of data
+def calculate_sha256(data):
+    sha256_hash = hashlib.sha256(data)
+    return sha256_hash.hexdigest()
+
+# Unpads chunk from zero filling
 def unpadding(data):
-    """
-    Funkcja usuwa wypełnienie zerami z danych.
-
-    :param data: Dane z wypełnieniem.
-    :return: Dane bez wypełnienia.
-    """
     return data.rstrip(b'\x00')
 
-# Zaimportowanie funkcji XOR
+# Decrypting chunk of data using xor
 def xor_decrypt(encrypted_chunk):
     key = encrypted_chunk[-256:]
     encrypted_bytes = encrypted_chunk[:-256]
@@ -34,20 +35,27 @@ def decrypt_authorization_header(packet):
             decrypted_bytes = xor_decrypt(encrypted_bytes)
             decrypted_bytes = unpadding(decrypted_bytes)
             
-            # Zapisanie zdeszyfrowanych danych do pliku binarnego
-            with open("decrypted_data.txt", "ab") as file:
-                file.write(decrypted_bytes)
+            # Return decrypted bytes
+            return decrypted_bytes
             
-            # print(f"Deszyfrowane dane dla pakietu {packet.summary()} zapisane do decrypted_data.bin")
     except Exception as e:
         print(f"Błąd podczas deszyfrowania pakietu {packet.summary()}: {e}")
 
 # Funkcja główna
 def main(pcap_file):
+    data = b''
     packets = scapy.rdpcap(pcap_file)
     for packet in packets:
         if scapy.Raw in packet:
-            decrypt_authorization_header(packet)
+            tmp = decrypt_authorization_header(packet)
+            if tmp:
+                data += tmp
+    # Compare Sofokles-Antygona.txt checksum with checksum calculated from decrypted data
+    if 'ef3c692e25809e17861bdcd44897ceecfb6881b6d9f0c9afa4aa7936fa7ac203' == calculate_sha256(data):
+        print("pcap is valid and contains ANTYGONA")
+    else:
+        print("pcap doesn't contain ANTYGONA")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send data via HTTP headers.')
